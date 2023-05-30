979f6b74e259b03e04a936e5b64476f42d5e69f7
Henry <anene.hekene@gmail.com>
2023-05-30 11:09:26 +0100

    minor refactor

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -4,6 +4,11 @@ import img from '../assets/image/notfound.svg';
 const Error = () => {
   return (
     <div className="h-screen w-full flex items-center justify-center ">
+      <nav className="top-0 mt-0 flex justify-between items-center w-full h-20  bg-gradient-to-b from-black to-gray-900 text-gray-200 fixed">
+        <div>
+          <h1 className="text-5xl font-signature  ml-2">Henry Anene</h1>
+        </div>
+      </nav>
       <div justify-center>
         <div>
           <img
